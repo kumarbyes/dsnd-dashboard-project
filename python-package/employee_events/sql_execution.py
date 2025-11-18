7a8df2820c66c19e5b7117fa1b32c98a1cc949dc
Srikrishnan Balasubramanian <kumar.byes@yahoo.com>
2025-11-18 18:30:06 +0100

    feat: Add QueryMixin

--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
-#### YOUR CODE HERE
-
+cwd = Path(__file__).parent
+db_path = cwd / 'employee_events.db'
 
 # OPTION 1: MIXIN
 # Define a class called `QueryMixin`
@@ -16,18 +16,29 @@ class QueryMixin:
     # that receives an sql query as a string
     # and returns the query's result
     # as a pandas dataframe
-    #### YOUR CODE HERE
+    def pandas_query(self,query_as_string):
+        con = connect(db_path)
+        cur = con.cursor()
+        cur.execute(query_as_string)
+        result = cur.fetchall()
+        con.close()
+        return pd.DataFrame(result)
+
 
     # Define a method named `query`
     # that receives an sql_query as a string
     # and returns the query's result as
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
-    #### YOUR CODE HERE
-    
+    def query(self,query_as_string):
+        con = connect(db_path)
+        cur = con.cursor()
+        cur.execute(query_as_string)
+        result = cur.fetchall()
+        con.close()
+        return result
 
- 
- # Leave this code unchanged
+# Leave this code unchanged
 def query(func):
     """
     Decorator that runs a standard sql execution
